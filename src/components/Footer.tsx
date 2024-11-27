613c046e2379ec6d08963ee67163246569d2f55f
tuyizereerneste <tuyizereernest@yahoo.com>
2024-11-27 19:05:14 +0200

    update socials

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -38,16 +38,16 @@ const Footer: React.FC = () => {
           <div>
             <h3 className="text-2xl font-bold mb-4">Follow Us</h3>
             <div className="flex space-x-4">
-              <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white">
+              <a href="https://www.facebook.com/profile.php?id=61560873492622&rdid=C0oVIAxa708B0bVH#" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white">
                 <FaFacebook size={24} />
               </a>
               <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white">
                 <FaTwitter size={24} />
               </a>
-              <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white">
+              <a href="https://www.instagram.com/help.linc/profilecard/?igsh=MW01ejZucWx1MmUzMA%3D%3D" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white">
                 <FaInstagram size={24} />
               </a>
-              <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white">
+              <a href="https://www.linkedin.com/in/helplinc/" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white">
                 <FaLinkedin size={24} />
               </a>
             </div>
